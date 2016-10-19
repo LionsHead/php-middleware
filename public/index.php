8820b31226c3b13d76622106866240f153f62179
Alexandr Savin <lion2head@gmail.com>
2016-10-19 17:49:36 +0400

    public path

--- public/index.php
+++ public/index.php
@@ -3,8 +3,7 @@
 define('APP_START_TIME', microtime(true));
 define('APP_START_MEMORY', memory_get_usage());
 
-define('DS', DIRECTORY_SEPARATOR);
-define('APP_PUBLIC', $_SERVER['DOCUMENT_ROOT'] . DS);
+define('APP_PUBLIC', $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR);
 // Основной домен без http\www и т.д.
 define('HOME_URL', 'localhost');
 
